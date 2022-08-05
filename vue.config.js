d5e944a4961dc9fac7b8e98e59b8525dae942134
Dohu <adc0612@gmail.com>
2022-08-05 15:21:28 +0900

    feat: Add build files

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: ['vuetify'],
+  publicPath: '/<repository_name>',
+  outputDir: 'docs',
 });
